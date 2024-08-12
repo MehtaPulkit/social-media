3853c46b6b744314b4acb916e6cc52d7c09be869
Pulkit Mehta <mehta.pulkit94@gmail.com>
2024-08-12 19:07:20 +1000

    User and Post backend files

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongodb.js";
 
